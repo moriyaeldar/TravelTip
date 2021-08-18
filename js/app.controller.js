cf99fc736ed4a44d5d3192886dcdf89fc6389065
moriyaeldar <75114672+moriyaeldar@users.noreply.github.com>
2021-08-18 22:55:21 +0300

    last

--- js/app.controller.js
+++ js/app.controller.js
@@ -102,7 +102,6 @@ function onGetUserPos() {
     getPosition()
         .then(pos => {
             console.log('User position is:', pos.coords);
-            // document.querySelector('.user-pos').innerText = `Latitude:${ pos.coords.latitude }-Longitude:${ pos.coords.longitude }`
 
             mapService.initMap(pos.coords.latitude, pos.coords.longitude)
 
