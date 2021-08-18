ea53c66facc0a2fdd67a5f132c0cf5366b1905c2
naamaark <naamaark@gmail.com>
2021-08-18 19:44:10 +0300

    add id to loc

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -9,7 +9,7 @@ const locs = [
     { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
     { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
 ]
-storageService.saveToStorage('locsDB',locs);
+storageService.saveToStorage('locsDB', locs);
 
 function getLocs() {
     const locs = storageService.loadFromStorage();
@@ -17,12 +17,13 @@ function getLocs() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(locs);
-            storageService.saveToStorage('locsDB',locs)
+            storageService.saveToStorage('locsDB', locs)
         }, 2000)
     });
 }
 
 function saveLoc(details) {
     locs.push(details)
-    storageService.saveToStorage('locsDB',locs)
+    details.id = (locs.length);
+    storageService.saveToStorage('locsDB', locs)
 }
